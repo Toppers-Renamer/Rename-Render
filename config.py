272c9af649e892ff7f53e9f5f14c82cf209cc4c3
Toppers-Renamer <renamer.robot@gmail.com>
2025-02-27 19:54:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "SuperToppersChannel") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "SuperToppersChannel,SuperToppersGroupp") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
